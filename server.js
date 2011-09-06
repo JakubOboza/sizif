ee8f3a97c88c48960bf9d3bc04377ea8c4692c34
JO <jakub.oboza@gmail.com>
2011-09-06 21:57:11 +0100

    small refactor

--- server.js
+++ server.js
@@ -1,50 +1,14 @@
-var sys   = require("sys"),
-    http  = require("http"),
-    url   = require("url"),
-    path  = require("path"),
-    fs    = require("fs");
-
-var Db = require('mongodb').Db,
-    Connection = require('mongodb').Connection,
-    Server = require('mongodb').Server,
-    GridStore = require('mongodb').GridStore;
-
-
+var Sizif = require("./lib/sizif");
+/*
 var database_name = process.env["SIZIF_DATABASE"] ? process.env["SIZIF_DATABASE"] : "panda_development" ;
 var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
 var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : Connection.DEFAULT_PORT;
 
-console.log("Connecting to Mongo at " + host + ":" + port);
-var mongo_db = new Db(database_name, new Server(host, port, {}), {native_parser:true});
+*/
 
-mongo_db.open(function(err, db) {
-  http.createServer(function(request, response) {
-        var uri = url.parse(request.url).pathname;
-        var filename = uri.substr(1); //path.join(process.cwd(), uri);
-
-
-          var gridStore = new GridStore(db, filename, "r");
-
-              gridStore.open(function(err, gridStore) {
-                gridStore.read(function(err, data){
-                  if(data){
-                    response.writeHead(200, {"Content-Type": gridStore.contentType});
-                    response.write(data, "binary");
-                    response.end();
-                  }else{
-                    response.writeHead(404, {"Content-Type": "text/plain"});
-                    response.end("File not found");
-                  }
-                });
-              //db.close();
-              //});
-       });
-
-    }).listen(8080);
-
+var sizif = new Sizif({
+    database: process.env["SIZIF_DATABASE"]
 });
-
-console.log("Server running at http://localhost:8080/");
 
 
 /*
